1a5ea998d72ac37d349c4d20a1f3e4c0c1d886f7
Luc Souilla <lucsouilla@gmail.com>
2025-06-08 16:35:26 -0400

    Install Quarto without sudo for Netlify

--- install-quarto.sh
+++ install-quarto.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 set -e
 
-echo "Installing Quarto..."
+echo "Installing Quarto without sudo..."
 
 QUARTO_VERSION=1.5.47
 
+# Create a local bin directory
+mkdir -p $HOME/local/bin
+
+# Download and extract the .deb package
 wget https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.deb
 
-sudo dpkg -i quarto-${QUARTO_VERSION}-linux-amd64.deb
+ar x quarto-${QUARTO_VERSION}-linux-amd64.deb
+tar -xJf data.tar.xz
 
+# Move the Quarto binary to a known path
+mv ./usr/bin/quarto $HOME/local/bin/
+
+# Update PATH
+export PATH=$HOME/local/bin:$PATH
+
+# Test
 quarto --version
