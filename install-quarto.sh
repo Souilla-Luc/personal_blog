ea9432a49df2b437d4e39f7ce9f3c005e0025de7
Luc Souilla <lucsouilla@gmail.com>
2025-06-08 16:27:18 -0400

    Add Quarto install script for Netlify

---
+++ install-quarto.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+QUARTO_VERSION="1.5.47"
+
+wget https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.deb
+sudo dpkg -i quarto-${QUARTO_VERSION}-linux-amd64.deb
+
+export PATH="/usr/local/bin:$PATH"
